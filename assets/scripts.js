b6631c89745a565910b3853afae29e104c732c0d
raphir <rhicode@gmail.com>
2017-03-15 15:55:38 +0100

    complete styling for project on all views. exported app data.

---
+++ assets/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".js-app-glossary-toggle").on("click", function(e) {
+        e.preventDefault();
+    })
+
+    // smooth scrolling 
+
+    
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        var target = $(this.hash);
+            if (target.length) {
+                $('html, body').animate({
+                scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+    });
+})
