272c2785294e2cb97bbdf0a278af33c9b7fc1b42
Aaron Zellweger <aaron.zellweger@gmail.com>
2020-05-20 10:14:48 +0200

    enhance build

--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -2,11 +2,10 @@ $(document).ready(function(){
   // smooth scrolling
   $('.app-glossary a[href*="#"]:not([href="#"])').click(function() {
     var target = $((this as any).hash);
-    console.log(target);
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 119 // fixed-header height
-        }, 500);
-      }
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top - 120 // fixed-header height
+      }, 500);
+    }
   });
 })
