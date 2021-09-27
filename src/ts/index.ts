da45f8efedb09d186e2edd1e09c46b6d532b6e2e
Maxim Hüsler <maxim.huesler@2sic.com>
2021-09-27 15:52:14 +0200

    (re)added hash feature

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,20 +2,37 @@ var winAny = window as any;
 winAny.appGlossary3 ??= {};
 winAny.appGlossary3.init ??= initGloassary3;
 
-
 function initGloassary3() {
-  // get navHight for correct scrollposition
+  // get navHeight for later use
   var nav = document.getElementsByTagName('header')[0];
   var navHeight = (nav != null ? nav.offsetHeight : 0);
 
-  let target = document.querySelector('[data-hash]')
-  target.addEventListener('click', () => {
-    const elemOffsetY = document.querySelector(`#${target.getAttribute('data-hash')}`).getBoundingClientRect().top + window.scrollY - navHeight;
+  document.querySelectorAll('.app-glossary3 a[href*="#"]:not([href="#"])').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      window.location.hash = this.getAttribute('href');
+
+      // substract scrollOffset with navHeight for accurate position 
+      const elemOffsetY = document.querySelector(this.getAttribute('href')).getBoundingClientRect().top + window.scrollY - navHeight;
+      // scroll to hash element
+      window.scrollTo({
+        top: elemOffsetY,
+        left: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  // get hash from url and open specific item
+  if (window.location.hash) {
+    // substract scrollOffset with navHeight for accurate position 
+    const elemOffsetY = document.querySelector(window.location.hash).getBoundingClientRect().top + window.scrollY - navHeight;
+    // scroll to hash element
     window.scrollTo({
       top: elemOffsetY,
       left: 0,
       behavior: 'smooth'
     });
-  });
-  
+  }
 }
